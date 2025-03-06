eeb859336839d3e78249c63a0027b99d42d989f8
aleconte1996 <alexleconte11@gmail.com>
2025-03-05 19:53:08 -0500

    updated spelling

--- server/controllers/timer.controller.js
+++ server/controllers/timer.controller.js
@@ -1,44 +1,53 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+// timer settings needs to be defined before route
+let timerSettings = {
+    sessionTimeEntry: 25,
+    breakTimeEntry: 5,
+    sessionRemainingSeconds: 1500,
+    breakRemainingSeconds: 300, 
+    running: false,
+    timerLabel: "Session", 
+}
 // GET - retrieve current timer settings
-router.get('/timer', (req, res) => {
-   res.json(timerSettings);
+router.get("/timer", (req, res) => {
+  res.json(timerSettings);
 });
 
 // PUT - update timer settings
-router.put('/timer', (req, res) => {
-    const { sessionTimeEntry, breakTimeEntry, moodScale } = req.body;
+router.put("/timer", (req, res) => {
+  const { sessionTimeEntry, breakTimeEntry, moodScale } = req.body;
 
-    if (sessionTimeEntry) {
-        timerSettings.sessionTime = sessionTimeEntry;
-        timerSettings.sessionRemaining = sessionTimeEntry * 60;
-    }
+  if (sessionTimeEntry) {
+    timerSettings.sessionTime = sessionTimeEntry;
+    timerSettings.sessionRemaining = sessionTimeEntry * 60;
+  }
 
-    if (breakTimeEntry) {
-        timerSettings.breakTimeEntry = breakTimeEntry;
-        timerSettings.breakRemaining = breakTimeEntry * 60;
-    }
-    res.json(timerSettings);
+  if (breakTimeEntry) {
+    timerSettings.breakTimeEntry = breakTimeEntry;
+    timerSettings.breakRemaining = breakTimeEntry * 60;
+  }
+  res.json(timerSettings);
 });
 
 // POST - Start/Stop timer
-router.post('/timer/startstop', (req, res) => {
-    timerSettings.running =!timerSettings.running;
-    res.json({ running: timerSettings.running });
+router.post("/timer/startstop", (req, res) => {
+  timerSettings.running = !timerSettings.running;
+  res.json({ running: timerSettings.running });
 });
 
 // DELETE - reset the timer
-router.delete('/timer/reset', (req, res) => {
-    timerSettings ={
-        sessionTimeEntry: 25,
-        breakTimeEntry: 5,
-        sessionRemainingSeconds: 1500,
-        breakRemainingSeconds: 300,
-        running: false,
-        timerLable: "Session", // could change this to ${USER INPUT}
-    };
-    res.json(timerSettings);
+router.delete("/timer/reset", (req, res) => {
+  timerSettings = {
+    sessionTimeEntry: 25,
+    breakTimeEntry: 5,
+    sessionRemainingSeconds: 1500,
+    breakRemainingSeconds: 300,
+    running: false,
+    timerLabel: "Session", // could change this to ${USER INPUT}
+  };
+  res.json(timerSettings);
 });
 
 module.exports = router;
