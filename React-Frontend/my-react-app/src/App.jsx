ce1a844a594d5a15bceba71fec6174f7cc13c3bb
TUnderwoo <tau4400@gmail.com>
2025-03-12 12:27:34 -0400

    log-in and sign-up converted to jsx

--- React-Frontend/my-react-app/src/App.jsx
+++ React-Frontend/my-react-app/src/App.jsx
@@ -1,5 +1,16 @@
-function App(){
+import Signup from "./components/authentication/sign-up";
 
+
+function App(){
+return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Login/>} />
+            <Route path="/" element={<Signup />} />
+        </Routes>
+    </Router>
+);
 }
 
 export default App;
