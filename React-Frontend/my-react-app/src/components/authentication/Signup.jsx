8526552e8e6230b978a700d5b327124d6b5c1126
TUnderwoo <tau4400@gmail.com>
2025-03-19 20:53:40 -0400

    debugging main

--- React-Frontend/my-react-app/src/components/authentication/Signup.jsx
+++ React-Frontend/my-react-app/src/components/authentication/Signup.jsx
@@ -71,6 +71,26 @@ function Signup() {
           />
         </div>
         <div>
+          <label htmlFor="lastname-input">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#e3e3e3"
+            >
+              <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z" />
+            </svg>
+          </label>
+          <input
+            type="text"
+            id="lastname-input"
+            placeholder="Last Name"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+        </div>
+        <div>
           <label htmlFor="email-input">@</label>
           <input
             type="email"
