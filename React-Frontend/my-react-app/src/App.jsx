e24e469d457deb5f0658b31eeb56fbccd9c2036b
TUnderwoo <tau4400@gmail.com>
2025-03-26 12:08:29 -0400

    slight change

--- React-Frontend/my-react-app/src/App.jsx
+++ React-Frontend/my-react-app/src/App.jsx
@@ -9,31 +9,31 @@ import Signup from "./components/authentication/Signup";
 import Dashboard from "./components/Dashboard.jsx";
 import ProtectedRoute from "./components/authentication/ProtectedRoutes"; 
 import TrackingTable from "./components/TrackingTable/TrackingTable.jsx"
-
 import Timer from "./components/timer/Timer";
+import DarkMode from "./components/DarkMode";
+
 
 function App() {
 
   return (
-    <Router>
-     
-      <Routes>
-        {/* Public routes */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+      <Router>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
-        {/* Protected routes */}
-        <Route element={<ProtectedRoute />}>
-        <Route path="/timer" element={<Timer />} />
-        <Route path="/progress" element={<TrackingTable />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          {/* Add other protected routes here */}
-        </Route>
+          {/* Protected routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/timer" element={<Timer />} />
+            <Route path="/progress" element={<TrackingTable />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            {/* Add other protected routes here */}
+          </Route>
 
-        {/* Redirect root to login page */}
-        <Route path="/" element={<Navigate to="/login" />} />
-      </Routes>
-    </Router>
+          {/* Redirect root to login page */}
+          <Route path="/" element={<Navigate to="/login" />} />
+        </Routes>
+      </Router>
   );
 }
 
